c00a2ab4c5d800d8bf893d711b9312fda8142247
mayank1dba <78435783+mayank1dba@users.noreply.github.com>
2021-09-27 20:59:16 +0800

    Add files via upload

---
+++ changelog.mysql.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+/* https://www.liquibase.org/documentation/sql_format.html */
+
+-- changeset activity.8:a8b.1
+create table person_a8b (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+-- rollback drop table person_a8b
